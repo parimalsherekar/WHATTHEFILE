85a0cfc23ef95e3f9afa844138fd206d753d6e5f
YashShewale <shewaleyashs123@gmail.com>
2025-04-02 19:53:45 +0530

    frontend pages added

--- frontend/script_download.js
+++ frontend/script_download.js
@@ -2,6 +2,11 @@ const params = new URLSearchParams(window.location.search);
 const roomid = params.get("roomid");
 const password = params.get("password");
 
+document.getElementById("rn").innerHTML = roomid;
+document.getElementById("rp").innerHTML = password;
+
+
+
 const download_card = document.getElementById("dc");
 const prog = document.getElementById('prog');
 
@@ -17,14 +22,14 @@ if (spotlight) {
     });
 }
 
-download_card.addEventListener('click',async()=>{
+download_card.addEventListener('click', async () => {
     prog.innerHTML = "Connecting to Sender";
 
-    const Response = await fetch("http://localhost:3000/getlink",{
-        method : "GET",
-        headers :{
-            roomid : roomid,
-            password : password
+    const Response = await fetch("http://192.168.178.135:3000/getlink", {
+        method: "GET",
+        headers: {
+            roomid: roomid,
+            password: password
         }
     });
     const data = await Response.json();
@@ -32,34 +37,34 @@ download_card.addEventListener('click',async()=>{
     const magnetLink = data.msg.link;
 
     console.log(magnetLink);
-    
+
     let dirHandle = null;
     const client = new WebTorrent();
     console.log("WebTorrent client created:", client);
     console.log("Adding torrent:", magnetLink);
-    
-    
+
+
     client.add(magnetLink, async (torrent) => {
         alert("Downloading:", torrent.name);
-        
+
         const file = torrent.files[0];
         console.log("Filename:", file.name);
-        
+
         try {
             dirHandle = await window.showDirectoryPicker();
             const fileHandle = await dirHandle.getFileHandle(file.name, { create: true });
             const writable = await fileHandle.createWritable();
-            
+
             const stream = file.createReadStream();
-            
-            
+
+
             for await (const chunk of stream) {
-                prog.innerHTML = Math.round(torrent.progress*100)+"%";
+                prog.innerHTML = Math.round(torrent.progress * 100) + "%";
                 await writable.write(chunk);
             }
 
             await writable.close();
-            prog.innerHTML =  "Download Completed";
+            prog.innerHTML = "Download Completed";
             alert(`Saved : ${file.name}`);
         } catch (err) {
             console.error("File save failed:", err);
